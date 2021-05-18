465cf728dad2a96e0ba6d92c43e3c3f428d65a12
Shpat Hyseni <shpat.hyseni@gmail.com>
2021-05-18 19:28:56 -0400

    Padding text and image fixed

--- src/components/AlbumThumbnail.jsx
+++ src/components/AlbumThumbnail.jsx
@@ -7,14 +7,11 @@ const AlbumThumbnail = props => {
 
         
                 
-        <section className="md:w-1/3 py-8 px-4">
-         
+        <section className="md:w-1/3 py-2 px-4">
             <div className="hidden md:block md:w-1/1 px-4 object-left">
-        
             {imagePath && <img className="rounded shadow-md " onClick={() => onTitleClicked(album.id) } src={imagePath} alt="" /> }
-            <h1 className="text-base md:text-lg font-semibold md:text-gray-500 capitalize" onClick={() => onTitleClicked(album.id) }>{album.title}</h1>
+            <h1 className="text-base pt-2 md:text-lg font-semibold md:text-gray-500 capitalize" onClick={() => onTitleClicked(album.id) }>{album.title}</h1>
             <p className="text-base md:text-md font-normal md:text-gray-500 capitalize">{album.photos.length} Photos</p>
-       
         </div>
        
         
